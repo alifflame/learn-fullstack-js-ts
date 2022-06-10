eef89f17760c7530cf9c69814d84e0b2d3e326cc
Alif Aunurrohim <alifaunurrohim@gmail.com>
2022-06-10 11:09:52 +0700

    install nextjs npm

---
+++ .next/server/pages/aboutus.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/aboutus";
+exports.ids = ["pages/aboutus"];
+exports.modules = {
+
+/***/ "./pages/aboutus.tsx":
+/*!***************************!*\
+  !*** ./pages/aboutus.tsx ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ AboutUs)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nfunction AboutUs(params) {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"p\", {\n        children: \"About Us\"\n    }, void 0, false, {\n        fileName: \"C:\\\\code\\\\code-nextjs\\\\learn-mern-ts\\\\pages\\\\aboutus.tsx\",\n        lineNumber: 3,\n        columnNumber: 9\n    }, this);\n};\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9hYm91dHVzLnRzeC5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQTtBQUFlLFNBQVNBLE9BQU8sQ0FBQ0MsTUFBVyxFQUFFO0lBQ3pDLHFCQUNJLDhEQUFDQyxHQUFDO2tCQUFDLFVBQVE7Ozs7O1lBQUksQ0FDakI7Q0FDTCIsInNvdXJjZXMiOlsid2VicGFjazovL2xlYXJuLW1lcm4tdHMvLi9wYWdlcy9hYm91dHVzLnRzeD9jMWY1Il0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IGZ1bmN0aW9uIEFib3V0VXMocGFyYW1zOnR5cGUpIHtcclxuICAgIHJldHVybiAoXHJcbiAgICAgICAgPHA+QWJvdXQgVXM8L3A+XHJcbiAgICApO1xyXG59Il0sIm5hbWVzIjpbIkFib3V0VXMiLCJwYXJhbXMiLCJwIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./pages/aboutus.tsx\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/aboutus.tsx"));
+module.exports = __webpack_exports__;
+
+})();
